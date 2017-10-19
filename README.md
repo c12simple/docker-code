f9025795af2f618fcc1604c7804df733524a1d3d
tran <c12simple@gmail.com>
2017-10-19 10:19:18 +0200

    push

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# docker -code
+It's clone version of https://github.com/CollaboraOnline/Docker-CODE/blob/master/Dockerfile
+Fix add the parameter to indicate "ipaddress domainName" into /etc/hosts of docker
---
+++ scripts/install-libreoffice.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Update installed packages
+apt-get update && apt-get -y upgrade
+
+# Install HTTPS transport
+apt-get -y install apt-transport-https
+
+# Install locales
+apt-get -y install locales-all
+
+# Install hyphenation patterns and spelling dictionaries
+apt-get -y install hyphen-* hunspell-hu hunspell-en-us hunspell-de-de-frami hunspell-nl hunspell-it
+
+# Add Collabora repos
+echo "deb https://collaboraoffice.com/repos/CollaboraOnline/CODE /" >> /etc/apt/sources.list.d/collabora.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6CCEA47B2281732DF5D504D00C54D189F4BA284D
+apt-get update
+
+# Install the Collabora packages
+apt-get -y install loolwsd code-brand
